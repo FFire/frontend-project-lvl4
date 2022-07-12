e28cb0aa5223bba846cc1bf1b6b8a86f3d3806ed
be1an <8497715@gmail.com>
2022-07-12 12:56:54 +0500

    fix: small

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,39 +1,40 @@
 import { LoginForm } from "../components/LoginForm.jsx";
 import logo from "../assets/Login.jpeg";
+import { Card, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export const LoginPage = (props) => (
-  <div class="h-100">
-    <div class="h-100" id="chat">
-      <div class="d-flex flex-column h-100">
-        <nav class="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
-          <div class="container">
-            <a class="navbar-brand" href="/">
+  <div className="h-100">
+    <div className="h-100" id="chat">
+      <div className="d-flex flex-column h-100">
+        <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
+          <div className="container">
+            <a className="navbar-brand" href="/">
               Hexlet Chat
             </a>
           </div>
         </nav>
-        <div class="container-fluid h-100">
-          <div class="row justify-content-center align-content-center h-100">
-            <div class="col-sm-11 col-md-10 col-lg-9 col-xl-8 col-xxl-7">
-              <div class="card shadow-sm">
-                <div class="card-body row p-5">
-                  <div class="col-12 col-md-6 d-flex align-items-center justify-content-center">
-                    <img src={logo} className="rounded-circle" alt="login" />
+        <Container className="h-100">
+          <div className="row justify-content-center align-content-center h-100">
+            <div className="col-sm-11 col-md-10 col-lg-9 col-xl-8 col-xxl-7">
+              <Card className="shadow-sm">
+                <Card.Body className="row px-lg-5">
+                  <div className="col-12 col-md-6 d-flex align-items-center justify-content-center">
+                    <Card.Img src={logo} className="rounded-circle" alt="login" />
                   </div>
-
                   <LoginForm />
-                </div>
-                <div class="card-footer p-4">
-                  <div class="text-center">
-                    <span>Нет аккаунта?</span> <a href="/signup">Регистрация</a>
+                </Card.Body>
+                <Card.Footer className="p-4">
+                  <div className="text-center">
+                    <span>Нет аккаунта?</span> <Link to="/signup">Регистрация</Link>
                   </div>
-                </div>
-              </div>
+                </Card.Footer>
+              </Card>
             </div>
           </div>
-        </div>
+        </Container>
       </div>
-      <div class="Toastify"></div>
+      <div className="Toastify"></div>
     </div>
   </div>
 );
